53841170ff147045eaf04269e3aed6fab89efa43
Srikar <srikar.rao001@gmail.com>
2019-04-18 23:36:31 -0700

    changed frequency keying

--- Transmission/experiments/wav.py
+++ Transmission/experiments/wav.py
@@ -1,7 +1,7 @@
 import wave, struct, math, random
 sampleRate = 44100.0 # hertz
-duration = 1 # seconds
-frequency = 440.0 # hertz
+duration = .048 * 5 # seconds
+frequency = 600.0 # hertz
 obj = wave.open('sound.wav','w')
 obj.setnchannels(1) # mono
 obj.setsampwidth(2)
@@ -13,44 +13,15 @@ def writeSinWave(freq, wav_obj):
 	   data = struct.pack('<h', value)
 	   wav_obj.writeframesraw( data )
 
-writeSinWave(frequency, obj)
-
-frequency = 900.0
-writeSinWave(frequency, obj)
-
-frequency = 10300.0
-writeSinWave(frequency, obj)
-
-frequency = 20000.0
-writeSinWave(frequency, obj)
-
-frequency = 3000.0
-writeSinWave(frequency, obj)
-
-frequency = 900.0
-writeSinWave(frequency, obj)
-
-frequency = 1300.0
-writeSinWave(frequency, obj)
-
-frequency = 2000.0
-writeSinWave(frequency, obj)
-
-frequency = 3000.0
-writeSinWave(frequency, obj)
 
 
-frequency = 900.0
-writeSinWave(frequency, obj)
-
-frequency = 1300.0
-writeSinWave(frequency, obj)
-
-frequency = 2000.0
-writeSinWave(frequency, obj)
-
-frequency = 30000.0
-writeSinWave(frequency, obj)
-
-
-
+writeSinWave(600, obj)
+writeSinWave(6000, obj)
+writeSinWave(600, obj)
+writeSinWave(6000, obj)
+writeSinWave(600, obj)
+writeSinWave(6000, obj)
+writeSinWave(600, obj)
+writeSinWave(6000, obj)
+writeSinWave(600, obj)
+writeSinWave(6000, obj)
--- Transmission/experiments/write_freq.py
+++ Transmission/experiments/write_freq.py
@@ -2,12 +2,12 @@ def write_freq_list(low, high, num_points):
 	#returns list of num_points frequencies from low to high, with intervals defined by polynomial ax^2 + low.
 
 	diff = high - low
-	a = diff/((num_points - 1) * ((num_points - 1)))
+	a = diff/((num_points - 1) ** 1.5)
 
-	arr = [low + a * (i * i) for i in range(num_points)]
+	arr = [low + a * (i ** 1.5) for i in range(num_points)]
 
 	f = open("frequencies.txt", "w")
 	for i in arr:
 		f.write(str(i) + "\r\n")
 	f.close()
-write_freq_list(500,7000,17	)
+write_freq_list(1500,7000,17)
