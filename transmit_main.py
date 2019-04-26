7ad762f3a75f3db925c38f4b4a76165c1ebb6320
Srikar <srikar.rao001@gmail.com>
2019-04-26 14:50:35 -0700

    changes to transmit main

--- transmit_main.py
+++ transmit_main.py
@@ -5,10 +5,15 @@ Main script to run for transmitting computer during final demo.
 from huffman_encode import *
 from error_correction_encode import *
 import os
+from text_to_sound import *
+from test import *
 
 dir_path = os.path.dirname(os.path.abspath(__file__)) + "/"
 
 if __name__ == "__main__":
-	huffman_encode.convert("tests/500_char.txt", "encoded/500_char_encode.txt", "frequency_table_2.txt")
-	error_correction_encode.berlekamp_welsh_encode(dir_path+"encoded/500_char_encode.txt", 4, dir_path+"error_encoded/500_char_error_encode.txt")
+	convert("tests/500_char.txt", "encoded/500_char_encode.txt", "frequency_table_2.txt")
+	berlekamp_welsh_encode(dir_path+"encoded/500_char_encode.txt", 7, dir_path+"error_encoded/500_char_error_encode.txt")
+	construct_wav("error_encoded/500_char_error_encode.txt", 1500, 7000, 131, make_wav_obj())
+	play_wav()
+
 	# transmit sound
