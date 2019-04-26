fbb438c9c62c631eb1fd335dd76859ec657fd725
Srikar <srikar.rao001@gmail.com>
2019-04-26 01:38:40 -0700

    minor bug fixes

--- huffman_encode.py
+++ huffman_encode.py
@@ -112,5 +112,7 @@ def convert(text_file, output_file, freq_file):
 
 if __name__ == "__main__":
 
-	convert("tests/short.txt", "encoded/short_encode.txt", "frequency_table_2.txt")
+	#convert("tests/short.txt", "encoded/short_encode.txt", "frequency_table_2.txt")
+	convert("tests/500_char.txt", "encoded/500_char_encode.txt", "frequency_table_2.txt")
+
 
